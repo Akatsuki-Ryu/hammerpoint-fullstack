b635c3b6928df6afc6bc94edb6e335d47aeab43c
akaboxmac <akabox@akabox.akabox>
2022-05-30 19:03:01 +0300

    add docker setup

---
+++ ssldocker/makedockerhttp.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout nginx.key -out nginx.crt
+ docker build -f Dockerfile.http -t nginx-ssl:latest .
+ docker run -p 8123:80 -p 8124:443 --name nginx-ssl -tid nginx-ssl
