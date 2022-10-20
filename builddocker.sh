528bb075f1730254582e7a34dc31d38345180fd3
akaboxmac <akabox@akabox.akabox>
2022-10-20 16:05:49 +0300

    fix docker build script

--- builddocker.sh
+++ builddocker.sh
@@ -1,5 +1,5 @@
 docker-compose build
-docker tag hammerpoint-fullstack_clientapp_prod:latest akabox165/hammerpoint-fe:latest
+docker tag hammerpoint-fullstack-clientapp_prod:latest akabox165/hammerpoint-fe:latest
 docker push akabox165/hammerpoint-fe:latest
-docker tag hammerpoint-fullstack_serverapp_debug:latest akabox165/hammerpoint-be:latest
+docker tag hammerpoint-fullstack-serverapp_debug:latest akabox165/hammerpoint-be:latest
 docker push akabox165/hammerpoint-be:latest
